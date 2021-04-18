ea1a5d48901243b7655633be8df4fd1419d3e2b4
Gazala malik <57172932+gazalamalik@users.noreply.github.com>
2021-04-18 14:32:12 +0530

    Add files via upload
    this is my first PHP project

---
+++ php_first_Project/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>first program of php </title>
+</head>
+<body>
+<?php
+
+//https://www.php.net/manual/en/function.strspn.php
+
+echo "am gazala malik";
+?>
+</body>
+</html>
+
